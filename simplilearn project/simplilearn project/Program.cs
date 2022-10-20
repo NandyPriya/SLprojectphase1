def58893c5f24e95e15593e065c380ac5af7e478
Nandhinipriya <nandhinipriya745@gmail.com>
2022-10-20 15:11:30 +0530

    Add project files.

---
+++ simplilearn project/simplilearn project/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+
+namespace simplilearn_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String a;
+            try
+            {
+                StreamReader g = new StreamReader("D:\\p1.txt");
+               
+                a = g.ReadLine();
+               
+                while (a != null)
+                {
+                    Console.WriteLine(a);
+                    
+                    a = g.ReadLine();
+                }
+                g.Close();
+               
+               
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Executing finally block.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
